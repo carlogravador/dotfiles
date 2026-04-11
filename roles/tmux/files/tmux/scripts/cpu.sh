03ddcd8c6dc567d6590b3397806a86ddbb5ffcb3
Carlo Gravador <cce.gravador@gmail.com>
2026-04-11 18:20:41 +0200

    Fix ram and cpu on linux tmux

--- roles/tmux/files/tmux/scripts/cpu.sh
+++ roles/tmux/files/tmux/scripts/cpu.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
-cpuvalue=$(ps -A -o %cpu | awk -F. 'NR>1{s+=$1} END{print s}')
-cpucores=$(sysctl -n hw.logicalcpu)
-echo "$((cpuvalue / cpucores))%"
+
+OS="$(uname)"
+
+if [ $OS = "Darwin" ]; then
+    cpuvalue=$(ps -A -o %cpu | awk -F. 'NR>1{s+=$1} END{print s}')
+    cpucores=$(sysctl -n hw.logicalcpu)
+    echo "$((cpuvalue / cpucores))%"
+elif [ "$OS" = "Linux" ]; then
+    cpucores=$(nproc)
+    cpu_idle=$(awk '/^cpu / {
+        idle=$5
+        total=0
+        for(i=2;i<=NF;i++) {
+            total+=$i
+        }
+        printf "%.0f", (idle / total) * 100
+    }' /proc/stat)
+
+fi
--- roles/tmux/files/tmux/scripts/ram.sh
+++ roles/tmux/files/tmux/scripts/ram.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/env bash
-used_pages=$(vm_stat | awk '
-  /Pages active:/              { a=$NF+0 }
-  /Pages wired down:/          { w=$NF+0 }
-  /Pages occupied by compressor:/ { c=$NF+0 }
-  /Pages speculative:/         { s=$NF+0 }
-  END { print a+w+c+s }
-')
-page_size=$(pagesize)
-used_mb=$((used_pages * page_size / 1048576))
-total_gb=$(sysctl -n hw.memsize | awk '{print int($1/1073741824)}')
 
-if [ "$used_mb" -lt 1024 ]; then
-  echo "${used_mb}MB/${total_gb}GB"
-else
-  echo "$((used_mb / 1024))GB/${total_gb}GB"
+OS=$(uname)
+if [ "$OS" = "Darwin" ]; then
+    used_pages=$(vm_stat | awk '
+    /Pages active:/              { a=$NF+0 }
+    /Pages wired down:/          { w=$NF+0 }
+    /Pages occupied by compressor:/ { c=$NF+0 }
+    /Pages speculative:/         { s=$NF+0 }
+    END { print a+w+c+s }
+    ')
+    page_size=$(pagesize)
+    used_mb=$((used_pages * page_size / 1048576))
+    total_gb=$(sysctl -n hw.memsize | awk '{print int($1/1073741824)}')
+
+    if [ "$used_mb" -lt 1024 ]; then
+    echo "${used_mb}MB/${total_gb}GB"
+    else
+    echo "$((used_mb / 1024))GB/${total_gb}GB"
+    fi
+elif [ "$OS" = "Linux" ]; then
+    mem_info=$(free -m | awk '/^Mem:/ {print $3, $2}')
+    used_mb=$(echo "$mem_info" | awk '{print $1}')
+    total_mb=$(echo "$mem_info" | awk '{print $2}')
+    total_gb=$((total_mb / 1024))
+
+    if [ "$used_mb" -lt 1024 ]; then
+        echo "${used_mb}MB/${total_gb}GB"
+    else
+        echo "$((used_mb / 1024))GB/${total_gb}GB"
+    fi
 fi
